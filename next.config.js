7aad8d9cf2a14ce23ca8a3988a86b0f7976953d0
alex7147gp <johncedillo77@gmail.com>
2022-09-24 16:44:30 -0500

    first commit

--- next.config.js
+++ next.config.js
@@ -1,9 +1,20 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-  images: {
-    domains: ['placeimg.com', 'api.lorem.space.com', 'vuzoon.com', 'www.complementosdelcafe.com'],
-  },
-}
 
-module.exports = nextConfig
+
+const nextConfig = {
+	reactStrictMode: true,
+    images: {
+      domains: ['placeimg.com',
+       'api.lorem.space.com',
+       'vuzoon.com',
+       'www.complementosdelcafe.com'
+      ],
+    },
+};
+const withPWA = require('next-pwa')({
+	dest: 'public',
+    disable: process.env.NODE_ENV === 'development',
+    register: true,  
+});
+
+module.exports = withPWA(nextConfig);
