fb1968e10a53e3d1a1465907eec8ac0ed113703e
alex7147gp <propani333@gmail.com>
2023-11-18 17:21:19 -0500

    [second commit]

--- src/components/Top.jsx
+++ src/components/Top.jsx
@@ -7,6 +7,16 @@ import styles from '../styles/Top.module.scss';
 
 const Topbar = ({menuOpen, setMenuOpen}) =>{ 
 
+  const toggleMenu = () => {
+    setMenuOpen(!menuOpen);
+  };
+
+  const handleKeyPress = (event) => {
+    if (event.key === 'Enter' || event.key === ' ') {
+      toggleMenu();
+    }
+  };
+
 	return(
     <div className={styles.wrapper}>
       <div className={styles.right}>
@@ -15,7 +25,12 @@ const Topbar = ({menuOpen, setMenuOpen}) =>{
           <spam className={styles.punto2} style={{color:`${menuOpen === true ? 'white':'black'}`}}></spam>
            <spam className={styles.punto3} style={{color:`${menuOpen === true ? 'white':'black'}`}}></spam>
         </div>        
-        <div className={styles.hamburger}>
+        <div 
+          className={styles.hamburger} 
+          onClick={toggleMenu}
+          onKeyPress={handleKeyPress}
+          tabIndex={0}
+          role={"button"}>
             <spam className={styles.linea1} style={{color:`${menuOpen === true ? 'white':'black'}`}}></spam >
             <spam className={styles.linea2} style={{color:`${menuOpen === true ? 'white':'black'}`}}></spam>
             <spam className={styles.linea3} style={{color:`${menuOpen === true ? 'white':'black'}`}}></spam>
